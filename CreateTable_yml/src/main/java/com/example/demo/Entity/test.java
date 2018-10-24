cff674abb60999b1957fc8e09d8220f8dc79915a
oborc <ji_ruitong@163.com>
2018-10-24 16:22:27 +0800

    一些test

---
+++ CreateTable_yml/src/main/java/com/example/demo/Entity/test.java
@@ -0,0 +1,15 @@
+package com.example.demo.Entity;
+
+import java.util.Scanner;
+
+public class test {
+    public static void main(String args[])
+    {
+        String s;
+
+        Scanner input = new Scanner(System.in);
+        s = input.next();
+
+    }
+
+}
