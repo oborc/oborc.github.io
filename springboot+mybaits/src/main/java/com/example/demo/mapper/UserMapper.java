efad32978727718e976088643df407caa159e679
oborc <ji_ruitong@163.com>
2018-12-19 15:31:10 +0800

    move spring web to new branch

--- springboot+mybaits/src/main/java/com/example/demo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mapper;
-
-import com.example.demo.entity.User;
-import tk.mybatis.mapper.common.BaseMapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-public interface UserMapper extends BaseMapper<User>,MySqlMapper<User>{
-}
--- springboot+mybaits/src/main/resources/db/migration/V1.0.0__demp-ddl.sql
+++
@@ -1,7 +0,0 @@
-create table user(
-id bigint unsigned auto_increment,
-age int ,
-name varchar(20),
-idcard varchar(18),
-primary key (id)
-)
