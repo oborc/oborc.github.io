3516a362f8e2762aeb90df7c3e5dbafb2567c3d5
oborc <ji_ruitong@163.com>
2021-04-07 09:13:44 +0800

    函数柯里化实现加法

---
+++ jsAlgorithm/curry.js
@@ -0,0 +1,24 @@
+function curry(fn){
+	// console.log(fn.length);
+	// console.log(arguments);
+	const ctx = this;
+	return function inner(...args){
+		console.log(arguments);
+		if(arguments.length == fn.length){
+			// console.log('this is inner this:'+this);
+			return fn.call(ctx,...args); // 返回结果
+		}else{
+			// console.log('this is inner ctx:' + ctx);
+			return inner.bind(ctx,...args); // 返回当前inner函数，并把参数带回
+		}
+	};
+}
+function add(x,y){
+	return x+y;
+}
+currAdd = curry(add);
+
+console.log("======================");
+console.log(currAdd(1)(2));
+console.log(currAdd(1,2));
+console.log(currAdd(1));
